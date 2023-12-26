5f7be2ba312902d755edbd535dcc00e81046b1a5
MuhammedDele <100720854+MuhammedDele@users.noreply.github.com>
2023-12-26 23:44:52 +0300

    adding products+adminstartion+urls

--- ecomprj/app/urls.py
+++ ecomprj/app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home),
-]
+    path('category/<slug:val>', views.CategoryView.as_view(),name="category"),
+    path('product-detail/<int:pk>', views.ProductDetails.as_view(),name="product-detail"),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- ecomprj/app/views.py
+++ ecomprj/app/views.py
@@ -1,5 +1,20 @@
+from django.db.models import Count
 from django.shortcuts import render
+from django.views import View
+
+from .models import Product
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        product = Product.objects.filter(category=val)
+        title= Product.objects.filter(category=val).values('title')
+        return render(request,"app/category.html",locals())
+    
+class ProductDetails(View):
+    def get(self,request,pk):
+        product = Product.objects.get(pk=pk) #beacuse we only need one product
+        return render(request,"app/productdetail.html",locals())
