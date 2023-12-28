abb3b9380851f523ff065e7943e0330c1c752921
MuhammedDele <100720854+MuhammedDele@users.noreply.github.com>
2023-12-28 12:21:42 +0300

    adding authentication

--- ecomprj/app/models.py
+++ ecomprj/app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 CATEGORY_CHOICES=(
@@ -18,3 +18,14 @@ class Product(models.Model):
     product_image = models.ImageField(upload_to='product')
     def __str__(self):
         return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField( max_length=100)
+    def __str__(self):
+        return self.name
